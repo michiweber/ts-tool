b6b125490aa8ad393f7e0a92ee5abd2386b5a89a
Michael Weber <michael.weber@valueminer.eu>
2017-10-19 10:22:08 +0200

    Random hex code generator: - Added copyright notice

--- src/tools/color.ts
+++ src/tools/color.ts
@@ -1,5 +1,14 @@
 export class Color {
 
+  /**
+   *
+   * Random hex code generator
+   * @copyright Paul Irish
+   * @link https://www.paulirish.com/2009/random-hex-color-code-snippets/
+   *
+   * @returns {string}
+   */
+
   public static randomHexGenerator() {
     return '#'+Math.floor(Math.random()*16777215).toString(16);
   }
